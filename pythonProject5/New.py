3c8e1940b6b1bc545dd904ce4de9a49fab308dd4
vladislavich245 <150582843+vladislavich245@users.noreply.github.com>
2024-03-03 07:18:46 +0200

    Update New.py

--- pythonProject5/New.py
+++ pythonProject5/New.py
@@ -5,7 +5,6 @@ class ImageEditor():
         self.filename = filename
         self.original = None
         self.changed = list()
-
     def open(self):
         try:
             self.original = Image.open(self.filename)
@@ -16,7 +15,23 @@ class ImageEditor():
         gray = self.original.convert('L')
         self.changed.append(gray)
         gray.save('gray.png')
+    def do_up(self):
+        up = self.original.transpose(Image.ROTATE_90)
+        self.changed.append(up)
+        up.save('up.png')
+    def do_mir(self):
+        mirr = self.original.transpose(Image.FLIP_LEFT_RIGHT)
+        self.changed.append(mirr)
+        mirr.save('mirr.png')
+    def do_bl(self):
+        gray = self.original.filter(ImageFilter.BLUR)
+        self.changed.append(gray)
+        gray.save('blur.png')
+
 
 MyImage = ImageEditor('Image.png')
 MyImage.open()
 MyImage.do_bw()
+MyImage.do_up()
+MyImage.do_mir()
+MyImage.do_bl()
